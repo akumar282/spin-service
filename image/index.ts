d03433194ef7e4f4261cb9e530df6e0d7b7d597a
akumar282 <abhi.kumar7567@gmail.com>
2025-02-04 01:38:32 -0800

    scrape" "

--- image/index.ts
+++ image/index.ts
@@ -1,15 +1,33 @@
-import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
-import ListNode from "./src/other";
-export const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
-  console.log(`Event: ${JSON.stringify(event, null, 2)}`);
-  console.log(`Context: ${JSON.stringify(context, null, 2)}`);
-  const what = new ListNode(3, null)
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'hello world', what
-    }),
-  };
-};
+import axios from 'axios'
+import { HTMLElement, parse as parseHTML } from 'node-html-parser'
 
-//test code
+const BASE_URL =
+  'https://www.reddit.com/svc/shreddit/community-more-posts/new/?name=AskReddit&adDistance=2&ad_posts_served=1&feedLength=4'
+
+async function getPage(endpoint: string): Promise<HTMLElement | number> {
+  try {
+    const data = await axios.get(endpoint, {
+      headers: {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'cache-control': 'no-cache',
+        "accept-encoding": "gzip, deflate, br",
+        "cookie": "intl_splash=false"
+      },
+    })
+    const parsedData = parseHTML(data.data);
+
+    return parsedData;
+  } catch (error) {
+    console.error('An error occurred:', error);
+    throw error;
+  }
+}
+
+async function testParse() {
+  const data = await getPage(BASE_URL)
+  const posts = (data as HTMLElement)?.querySelectorAll('article[class="w-full m-0"]')
+  console.log(posts)
+}
+
+testParse().then()
