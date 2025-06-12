b059a344078da07f7b1ad587076db230f352ac66
akumar282 <abhi.kumar7567@gmail.com>
2025-06-12 04:21:59 -0700

    figuring out

--- ec2-proxy/index.ts
+++ ec2-proxy/index.ts
@@ -6,23 +6,10 @@ const port = 8080
 
 
 app.use(/(.*)/, (req: Request, res: Response) => {
-  console.log("catch all")
-  console.log(req.path)
-  res.send(`Hello ${req.path}`)
+  const auth = req.headers.authorization
+  res.send(`Hello ${req.method}, ${req.originalUrl}, ${req.host}`)
 })
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('<!DOCTYPE html>\n' +
-    '<html>\n' +
-    '<body>\n' +
-    '\n' +
-    '<h1>My First Heading</h1>\n' +
-    '\n' +
-    '<p>My first paragraph.</p>\n' +
-    '\n' +
-    '</body>\n' +
-    '</html>')
-})
 
 app.listen(port, () => {
   console.log(`Server started on ${port}`)
