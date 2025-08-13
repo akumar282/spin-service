9a702c429cfeb324ec7140e7fc47c927ed46155f
akumar282 <abhi.kumar7567@gmail.com>
2025-08-12 20:52:21 -0700

    save point

--- infrastructure/lib/spin-client-stack.ts
+++ infrastructure/lib/spin-client-stack.ts
@@ -1,8 +1,23 @@
-import { Stack, StackProps } from 'aws-cdk-lib'
+import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib'
 import { Construct } from 'constructs'
+import { Bucket, BucketEncryption } from 'aws-cdk-lib/aws-s3'
+import { Distribution } from 'aws-cdk-lib/aws-cloudfront'
+import { S3BucketOrigin } from 'aws-cdk-lib/aws-cloudfront-origins'
 
 export class SpinClientStack extends Stack {
   public constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props)
+
+    const deploymentBucket = new Bucket(this, 'DeploymentBucketSpinClient', {
+      bucketName: 'DeploymentBucketSpinClient',
+      encryption: BucketEncryption.S3_MANAGED,
+      removalPolicy: RemovalPolicy.DESTROY,
+    })
+
+    const cloudfrontDistro = new Distribution(this, 'SpinClientDistribution', {
+      defaultBehavior: {
+        origin: S3BucketOrigin.withOriginAccessControl(deploymentBucket),
+      },
+    })
   }
 }
