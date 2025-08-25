c7771ea3c1f259e682c57a080e71fa59f10571e8
akumar282 <abhi.kumar7567@gmail.com>
2025-08-25 04:56:50 -0700

    clear sizing

--- spin-web-client/app/components/Card.tsx
+++ spin-web-client/app/components/Card.tsx
@@ -4,16 +4,15 @@ interface CardProps {
   color: string
   genre: string[]
   storeLink: string
-
 }
 
 export function Card() {
   return (
-    <div className="my-40 bg-white/30 font-primary rounded-3xl mx-auto shadow-2xl">
-      <div className="w-full h-full flex lg:flex-row items-center flex-col">
+    <div className=" bg-white/30 font-primary rounded-3xl shadow-2xl">
+      <div className="flex lg:flex-row items-center flex-col">
         <img alt="cover" className="lg:w-40 rounded-2xl lg:h-40 h-44 w-44 m-3"
              src='https://i.discogs.com/3jIXeeFG8I_JDCJLzD2WTpatabGHMeHqOBxjJibqU1A/rs:fit/g:sm/q:40/h:150/w:150/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTEzODU1/MDE5LTE2MzYxMjI1/NzctMTA1NC5qcGVn.jpeg' />
-        <div className="flex my-auto flex-col mx-auto">
+        <div className="flex flex-col">
           <h1 className="text-xl pb-2">
             Mac Demarco - Old Dog Demos
           </h1>
--- spin-web-client/app/modules/home/home.tsx
+++ spin-web-client/app/modules/home/home.tsx
@@ -3,7 +3,7 @@ import { Card } from '~/components/Card'
 export function Home() {
   return (
     <main className="flex flex-col bg-gradient-to-tl from-lime-200 via-sky-200 to-indigo-300 min-h-screen">
-      <div className="items-center justify-center grid lg:grid-cols-3 grid-cols-1">
+      <div className="items-center justify-center h-3/12 grid lg:grid-cols-3 gap-3 grid-cols-1">
         <Card/>
         <Card/>
         <Card/>
