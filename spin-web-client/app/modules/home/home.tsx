11f63a7b9687511529dce763318d655ada06d6a1
akumar282 <abhi.kumar7567@gmail.com>
2025-08-25 06:40:56 -0700

    space

--- spin-web-client/app/modules/home/home.tsx
+++ spin-web-client/app/modules/home/home.tsx
@@ -2,8 +2,8 @@ import { Card } from '~/components/Card'
 
 export function Home() {
   return (
-    <main className="flex flex-col bg-gradient-to-tl from-lime-200 via-sky-200 to-indigo-300 min-h-screen">
-      <div className="items-center justify-center h-3/12 grid lg:grid-cols-3 gap-3 grid-cols-1">
+    <main className="flex flex-col bg-gradient-to-tl items-center from-lime-200 via-sky-200 to-indigo-300 min-h-screen">
+      <div className="items-center justify-center w-11/12 grid lg:grid-cols-3 gap-3 grid-cols-1">
         <Card/>
         <Card/>
         <Card/>
