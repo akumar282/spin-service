1266f556b2ecca2b27c6c9e6dd877f795eefadcf
akumar282 <abhi.kumar7567@gmail.com>
2025-08-29 14:21:23 -0700

    ref

--- spin-web-client/app/routes/signup.tsx
+++ spin-web-client/app/routes/signup.tsx
@@ -1,13 +1,13 @@
-import type { Route } from "./+types/home";
-import { SignUpComponent } from '~/modules/auth/signup';
+import type { Route } from './+types/home'
+import { SignUpComponent } from '~/modules/auth/signup'
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "spin-service" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+    { title: 'spin-service' },
+    { name: 'description', content: 'Welcome to React Router!' },
+  ]
 }
 
 export default function SignUp() {
-  return <SignUpComponent />;
+  return <SignUpComponent />
 }
