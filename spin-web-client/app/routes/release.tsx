7c36ac893a6d50036b226d6c2c5a2d0eb764970c
akumar282 <abhi.kumar7567@gmail.com>
2025-11-21 03:26:29 -0800

    style

--- spin-web-client/app/routes/release.tsx
+++ spin-web-client/app/routes/release.tsx
@@ -10,6 +10,7 @@ import { SpinClient } from '~/api/client'
 import { generateTags } from '~/components/Card'
 import { AuthContext } from '~/components/AuthContext'
 import { cap, updateUser } from '~/functions'
+import { Notation } from '~/components/Notation'
 
 export default function ReleasePage() {
   const location = useLocation()
@@ -74,6 +75,8 @@ export default function ReleasePage() {
     await updateUser(userContext, client, update)
   }
 
+  const notation = Notation(data.releaseType, data.preorder, 2)
+
   return (
     <main>
       <div
@@ -81,42 +84,49 @@ export default function ReleasePage() {
         <HomeNavbar/>
         <div className='w-full items-center max-w-[116rem] pt-3 flex flex-col'>
           <div
-            className='w-[98%] justify-between my-0.5 rounded dark:bg-slate-300 dark:text-black dark:border-indigo-600 rounded-xl bg-white flex flex-row border border-slate-400 items-stretch'>
-            <div className='flex w-full lg:flex-row md:flex-row flex-col'>
-              <div className='flex items-center justify-center lg:m-3 m-3'>
-                <div className='h-[180px] w-[180px] lg:w-[220px] lg:h-[220px] flex-shrink-0'>
-                  <img
-                    className='h-full w-full object-cover rounded'
-                    src={data.thumbnail && data.thumbnail !== '' ? data.thumbnail : alternateImage}
-                    alt='title'
-                  />
+            className='w-[98%] justify-between my-0.5 rounded dark:bg-slate-300 dark:text-black dark:border-indigo-600 rounded-xl bg-white flex flex-col border border-slate-400 items-stretch'>
+            <div>
+              {notation}
+            </div>
+            <div className='flex w-full justify-center'>
+              <div className='mx-2 lg:w-full md:w-full flex grow flex-col'>
+                <div className='flex flex-col lg:flex-row md:flex-row'>
+                  <div className='flex flex-col items-center justify-center lg:m-3 m-3'>
+                    <div className='h-[200px] w-[200px] md:h-[200px] md:w-[200px] lg:w-[220px] lg:h-[220px] flex-shrink-0'>
+                      <img
+                        className='h-full w-full object-cover rounded'
+                        src={data.thumbnail && data.thumbnail !== '' ? data.thumbnail : alternateImage}
+                        alt='title'
+                      />
+                    </div>
+                  </div>
+                  <div className='lg:ml-2'>
+                    <h1 className='lg:text-2xl text-xl mt-3'>
+                      {data.album}
+                    </h1>
+                    <h3 className='lg:text-xl text-lg mb-3  text-wrap'>
+                      {data.artist}
+                    </h3>
+                    <h3 className='lg:text-md text-sm italic  text-wrap'>
+                      {'Release'}
+                    </h3>
+                    <h3 className='lg:text-md text-sm italic text-wrap'>
+                      {data.year}
+                    </h3>
+                    <h3 className='lg:text-md text-sm text-smitalic text-wrap'>
+                      Format: {cap(data.media)}
+                    </h3>
+                    <h3 className='lg:text-md text-sm italic text-wrap'>
+                      Color: {cap(data.color)}
+                    </h3>
+                    <h3 className='lg:text-md text-sm text-blue-700 mt-2'>
+                      <a target='_blank' title={'View on Discogs'} href={'https://discogs.com' + data.uri}
+                         rel='noreferrer'>View full information
+                        on Discogs</a>
+                    </h3>
+                  </div>
                 </div>
-              </div>
-              <div className='lg:m-5 m-2 lg:w-full md:w-full flex flex-col'>
-                <h1 className='text-lg'>
-                  {data.album}
-                </h1>
-                <h3 className='text-md mb-3  text-wrap'>
-                  {data.artist}
-                </h3>
-                <h3 className='text-sm italic  text-wrap'>
-                  {'Release'}
-                </h3>
-                <h3 className='text-sm italic text-wrap'>
-                  {data.year}
-                </h3>
-                <h3 className='text-sm italic text-wrap'>
-                  Format: {cap(data.media)}
-                </h3>
-                <h3 className='text-sm italic text-wrap'>
-                  Color: {cap(data.color)}
-                </h3>
-                <h3 className='text-sm text-blue-700 mt-2'>
-                  <a target='_blank' title={'View on Discogs'} href={'https://discogs.com' + data.uri}
-                     rel='noreferrer'>View full information
-                    on Discogs</a>
-                </h3>
-                <div className='lg:m-5 flex flex-wrap mt-3 justify-center gap-2'>
+                <div className='my-3 flex flex-wrap justify-center gap-2'>
                   {data.genre ?
                     data.genre.map((x: string, index: number) => generateTags(x, index)) : <></>
                   }
@@ -135,7 +145,7 @@ export default function ReleasePage() {
                     Notify me for this album
                   </button>
                   <button onClick={() => addArtist(artistData)}
-                          className='dark:bg-indigo-300 border-2 border-indigo-600 bg-orange-300 text-md rounded-xl w-full py-0.5 dark:hover:bg-indigo-500'>
+                          className='dark:bg-indigo-300 border-2 border-indigo-600 bg-orange-300 text-md rounded-xl w-full py-0.5 lg:px-0 px-1.5 dark:hover:bg-indigo-500'>
                     Notify me this artists releases
                   </button>
                 </div>
